8156080b81f718a1a967e9b732d4c25234f35964
lintingting_sara@163.com <tingting.b.lin@accenture.com>
2020-10-23 15:28:26 +0800

    On branch master

---
+++ bip.js
@@ -0,0 +1,12 @@
+//* string
+const name = "John"
+const age = 30
+//*concatenation
+console.log("My name is " + name +" and age is " + age)
+//template
+console.log(`My name is ${name} and age is ${age}`)
+ const s = 'Hello';
+ console.log(s.length)
+
+ const numbers = new Array(1,2,3,4,5)
+ console.log(numbers)
